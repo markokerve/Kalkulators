b15f74967c00438cfd85906e225b51551ea2adbd
markokerve <147307158+markokerve@users.noreply.github.com>
2024-05-16 09:52:59 +0300

    Update kalkulators.py

--- kalkulators.py
+++ kalkulators.py
@@ -9,6 +9,7 @@ def btnClick(number):
     newNumber=str(current)+str(number)
     e.insert(0,newNumber)#ievieto displejā
     return 0
+
 def btnCommand(command):
     global num1
     global mathOp
@@ -16,18 +17,71 @@ def btnCommand(command):
     e.delete(0,END)
     return 0
 
-btn0=Button(mansLogs,text='0',padx='40',pady='20')
-btn1=Button(mansLogs,text='1',padx='40',pady='20')
-btn2=Button(mansLogs,text='2',padx='40',pady='20')
-btn3=Button(mansLogs,text='3',padx='40',pady='20')
-btn4=Button(mansLogs,text='4',padx='40',pady='20')
-btn5=Button(mansLogs,text='5',padx='40',pady='20')
-btn6=Button(mansLogs,text='6',padx='40',pady='20')
-btn7=Button(mansLogs,text='7',padx='40',pady='20')
-btn8=Button(mansLogs,text='8',padx='40',pady='20')
-btn9=Button(mansLogs,text='9',padx='40',pady='20')
-e=Entry(mansLogs,width=15,bd=10,font=('Arial Black', 20),justify='right')
+def btnClick(number):
+    current=e.get()#nolasa skaitli
+    e.delete(0,END)#nodzēš
+    newNumber=str(current)+str(number)
+    e.insert(0,newNumber)#ievieto displejā
+    return 0
 
+def btnCommand(command):
+    global number
+    global num1 #jāiegaumē skaitlis, darbība
+    global mathOp#matemātiskais operators
+    mathOp=command
+    num1=int(e.get())
+    e.delete(0,END)
+    return 0
+
+def Vienads():
+    num2=int(e.get())
+    result=0
+    if mathOp=="+":
+        result=num1+num2
+    elif mathOp=="-":
+        result=num1-num2
+    elif mathOp=="*":
+        result=num1*num2
+    elif mathOp=="/":
+        result=num1/num2
+    else:
+        result=0
+    e.delete(0,END)
+    e.insert(0,str(result))
+    return 0
+
+def notirit():
+    e.delete(0,END)
+    num1=0
+    mathOp=""
+    return 0
+
+
+
+btnVien=Button(mansLogs,text='=',padx='40',pady='20',bg='cyan',command=Vienads)
+btnReiz=Button(mansLogs,text='*',padx='40',pady='20',bg='cyan',command=lambda:btnCommand('*'))
+btnDal=Button(mansLogs,text='/',padx='40',pady='20',bg='cyan',command=lambda:btnCommand('/'))
+btnMinus=Button(mansLogs,text='-',padx='40',pady='20',bg='cyan',command=lambda:btnCommand('-'))
+btnSask=Button(mansLogs,text='+',padx='40',pady='20',bg='cyan',command=lambda:btnCommand('+'))
+btn0=Button(mansLogs,text='0',padx='40',pady='20',bg='red',command=lambda:btnClick(0))
+btn1=Button(mansLogs,text='1',padx='40',pady='20',bg='red',command=lambda:btnClick(1))
+btn2=Button(mansLogs,text='2',padx='40',pady='20',bg='red',command=lambda:btnClick(2))
+btn3=Button(mansLogs,text='3',padx='40',pady='20',bg='red',command=lambda:btnClick(3))
+btn4=Button(mansLogs,text='4',padx='40',pady='20',bg='red',command=lambda:btnClick(4))
+btn5=Button(mansLogs,text='5',padx='40',pady='20',bg='red',command=lambda:btnClick(5))
+btn6=Button(mansLogs,text='6',padx='40',pady='20',bg='red',command=lambda:btnClick(6))
+btn7=Button(mansLogs,text='7',padx='40',pady='20',bg='red',command=lambda:btnClick(7))
+btn8=Button(mansLogs,text='8',padx='40',pady='20',bg='red',command=lambda:btnClick(8))
+btn9=Button(mansLogs,text='9',padx='40',pady='20',bg='red',command=lambda:btnClick(9))
+e=Entry(mansLogs,width=15,bd=10,font=('Arial Black', 20),justify='right')
+btnC=Button(mansLogs,text='C',padx='40',pady='20',bg='cyan',command=notirit)
+
+btnVien.grid(row=4,column=2)
+btnReiz.grid(row=4,column=4)
+btnDal.grid(row=1,column=4)
+btnMinus.grid(row=3,column=4)
+btnSask.grid(row=2,column=4)
+btnC.grid(row=4,column=1)
 btn7.grid(row=1,column=0)
 btn8.grid(row=1,column=1)
 btn9.grid(row=1,column=2)
